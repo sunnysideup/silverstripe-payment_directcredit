5cccb7f060b5aa2795976c7f32189442df0fdcd4
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-09-27 04:19:02 +0000

    added example to _config
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_directcredit/trunk@1524 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -6,5 +6,9 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START payment_directcredit MODULE ----------------===================
+//MUST ADD - you may also add other methods
+//Payment::set_supported_methods(array('DirectCreditPayment' => 'Direct Credit Payment'));
+
+//MAY ADD
 //DirectCreditPayment::set_custom_message_for_direct_credit("your bank account here");
 //===================---------------- END payment_directcredit  MODULE ----------------===================
