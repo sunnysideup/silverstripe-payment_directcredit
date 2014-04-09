a0dcc38000925b6ac477416883f9abc3641cf4f5
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-04-09 04:40:25 +0000

    UPGRADE 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_directcredit/trunk@4929 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -3,8 +3,3 @@
  * @author nicolaas [at] www.sunnysideup.co.nz
 **/
 
-
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START payment_directcredit MODULE ----------------===================
-
-//===================---------------- END payment_directcredit  MODULE ----------------===================
